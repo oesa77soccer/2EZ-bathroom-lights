c9e28f33cf1917e281c3735e806090606eb80851
oesa77soccer <oesa77soccer@gmail.com>
2021-08-26 18:21:51 -0400

    slider shell created

--- src/index.php
+++ src/index.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html>
 <body>
+<link rel="stylesheet" type="text/css" href="/var/www/html/2EZ-bathroom-lights/src/css/style.css">
+<script src="/var/www/html/2EZ-bathroom-lights/src/js/javascript-stuff.js"></script>
 
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
   <input type="submit" value="rainbow" name="fname">
 </form>
+
+<div class="slidecontainer">
+  <input type="range" min="0" max="255" value="0" class="slider" id="myRange">
+</div>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
